6c0ae9b41e449b419305e36eb8370199bada5d14
Maria Elena Martinez <mariaelenamartinez1a@gmail.com>
2022-09-21 20:31:17 -0300

    Guia1/ejercicio11_guia1.py

---
+++ Guia 1/ejercicio11_guia1.py
@@ -0,0 +1,7 @@
+"""Ahorros. Escribir un programa en el cual muestre a fin de año el total de ahorro obtenido, si se pide en cada mes el 10% del sueldo ganado.
+"""
+print("Calcularemos su ahorro desde enero hasta diciembre".upper())
+sueldo= float(input("Ingrese el monto ganado= "))
+
+for i in range(12):
+    
