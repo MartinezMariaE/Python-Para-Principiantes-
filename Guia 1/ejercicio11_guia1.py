df2f77bbafc0eee71062d9edc8f9ce18a4b76401
Maria Elena Martinez <mariaelenamartinez1a@gmail.com>
2022-09-21 21:24:55 -0300

    Guia 1/ejercicio11_guia1.py

--- Guia 1/ejercicio11_guia1.py
+++ Guia 1/ejercicio11_guia1.py
@@ -1,7 +1,16 @@
 """Ahorros. Escribir un programa en el cual muestre a fin de año el total de ahorro obtenido, si se pide en cada mes el 10% del sueldo ganado.
 """
-print("Calcularemos su ahorro desde enero hasta diciembre".upper())
-sueldo= float(input("Ingrese el monto ganado= "))
+contador=0
+sueldo= float()
+acumulador=0.0
+porcentaje=0.0
 
-for i in range(12):
-    
+print("Ahorro desde Enero a Diciembre")
+sueldo= int(input("Ingrese su sueldo "))
+while(contador<= 11):
+    porcentaje=(sueldo*0.10)
+    acumulador=acumulador+porcentaje
+    contador=contador+1
+    print("Mes ", contador," tiene un ahorro de ",acumulador )
+print("A la derecha puede visualizar los ahorros de ese mes, mas el mes anterior ya sumados.")
+print("Finalmente, su ahorro del año fue de: ",acumulador)
