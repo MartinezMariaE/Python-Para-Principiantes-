fdd362510c2503e8320ff62b5c3e0d2c604d247d
mariaelena08 <mariaelenamartinez1a@gmail.com>
2022-09-23 20:59:49 -0300

    ejercicio17_guia1.py

---
+++ ejercicio17_guia1.py
@@ -0,0 +1,3 @@
+"""
+ Galería de Arte Una galería de arte desea preparar un catálogo de sus cuadros más famosos. Se realiza una prueba con tres cuadros y por cada uno se ingresa el año en que fue creado. El programa deberá verificar si todos los cuadros son anteriores al siglo XX (El siglo XX es el siglo pasado. Se inició en el año 1901 y terminó en el año 2000). Determinar cuántos tienen antigüedad inferior a 10 años. Si no hay ninguno, imprimir el mensaje “Renovar stock”.
+"""
