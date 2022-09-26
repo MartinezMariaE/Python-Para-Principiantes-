5625b4aa6a24ea18af3a005c8573cc929cf1bd9f
mariaelena08 <mariaelenamartinez1a@gmail.com>
2022-09-26 18:44:08 -0300

    Guia 1/ejercicio16_guia1.py Guia 1/ejercicio17_guia1.py Guia 1/ejercicio18_guia1.py Guia 1/ejercicio19_guia1.py

---
+++ Guia 1/ejercicio18_guia1.py
@@ -0,0 +1,3 @@
+"""
+Tarjeta de Bingo Realizar un programa que genere 15 numero aleatorios enteros en el rango del 1 al 100, que representaria la tarjeta de bingo de una persona. Una vez generado los numeros aleatorios solicitar al usuario que ingrese 3 numeros enteros, a parƟr de alli mostrar los siguientes mensajes: Si el usuario no marco ninguno de los numeros indicarlo diciendo “El jugador Ɵene mala suerte, no marco ninguna casilla”. Caso contrario mostrar “El jugador marco algun numero de la tarjeta”
+"""
